cd5e3363bad18b7d74fad6ff19a01e66e85165ab
rabinhood-afg <hmorteza807@gmail.com>
2025-01-10 04:52:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+// toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active'); 
+
+
+
